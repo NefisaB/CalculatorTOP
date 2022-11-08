b8b6aa3e64e84f1c1b9050cd90a32ce018e00d5b
NefisaB <nefisa.hc@gmail.com>
2022-11-08 10:32:37 +0100

    Add functionality for keyboard buttons.

--- app.js
+++ app.js
@@ -51,6 +51,23 @@ function operate(operator, num1, num2) {
     return result;
 }
 
+function evaluateExpression() {
+    if (!operands[0] || operands[1] === "") {
+        console.log("if");
+        operands[0] = currentValue.textContent;
+        operands[1] = this.id;
+        expression.textContent = operands[0] + operands[1];
+        currentValue.textContent = "";
+    } else {
+        operands[2] = currentValue.textContent;
+        operands[0] = operate(operands[1], operands[0], operands[2]);
+        console.log("else");
+        expression.textContent = operands[0] + this.id;
+        currentValue.textContent = "";
+        operands[1] = this.id;
+    }
+}
+
 numBtns.forEach(numBtn => {
     numBtn.addEventListener("click", function () {
         currentValue.textContent += this.id;
@@ -58,29 +75,40 @@ numBtns.forEach(numBtn => {
 });
 
 operatorBtns.forEach(operBtn => {
-    operBtn.addEventListener("click", function () {
-        if (!operands[0]) {
-            operands[0] = currentValue.textContent;
-        }
-        expression.textContent = operands[0];
-        currentValue.textContent = "";
-        operands[1] = this.id;
-        expression.textContent += this.id;
-    });
+    operBtn.addEventListener("click", evaluateExpression);
 });
+
+
 
 evalBtn.addEventListener("click", function () {
     operands[2] = currentValue.textContent;
     expression.textContent += operands[2];
-    console.log(operands);
     currentValue.textContent = operate(operands[1], operands[0], operands[2]);
     operands[0] = currentValue.textContent;
+    operands[1] = "";
 });
 
 clearBtn.addEventListener("click", function () {
     operands = [];
     currentValue.textContent = "";
     expression.textContent = "";
+});
+
+document.addEventListener("keypress", function (e) {
+    const val = e.key;
+    if (val >= 0 && val < 9) {
+        currentValue.textContent += val;
+    }
+    if (["+", "-", "*", "/"].includes(val)) {
+        for (let btn of operatorBtns) {
+            if (val === btn.id) {
+                btn.click();
+            }
+        }
+    }
+    if (val === "Enter") {
+        evalBtn.click();
+    }
 });
 
 
